2636bdbfdebb6712ad78fd00ea934ca079f7e548
VZGLY <chaineux.gavin@gmail.com>
2022-10-26 11:07:13 +0200

    Ajoutez des fichiers projet.

---
+++ MaPremiereAppMVC/Context/FakeDb.cs
@@ -0,0 +1,28 @@
+﻿namespace MaPremiereAppMVC.Context
+{
+    public static class FakeDb
+    {
+        public static List<User> Users { get; set; } = new List<User>()
+        {
+            new User(1, "Popol@gmail.com", "Popol93", "Test123="),
+            new User(2, "Pupul@gmail.com", "Pupul91", "Test123="),
+            new User(3, "Papal@gmail.com", "Papal1000", "Test123=")
+        };
+    }
+
+    public class User
+    {
+        public User(int id, string email, string username, string password)
+        {
+            Id = id;
+            Email = email;
+            UserName = username;
+            Password = password;
+        }
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+    }
+}
---
+++ MaPremiereAppMVC/Controllers/AdminController.cs
@@ -0,0 +1,72 @@
+﻿using MaPremiereAppMVC.Models;
+using MaPremiereAppMVC.Models.ModelViews;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MaPremiereAppMVC.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return RedirectToAction("Bienvenu");
+        }
+
+        [Route("Admin/new")]
+        [Route("NewAdmin")]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Bienvenu()
+        {
+            AdminBienvenuView model = new AdminBienvenuView() {
+                AdminContactMail = "info.admin@monSiteMVC.be"
+            };
+            model.AddAdmin(new Admin() { 
+                FirstName = "Samuel", 
+                LastName = "Legrain", 
+                Email = "samuel.legrain@bstorm.be"
+            });
+            model.AddAdmin(new Admin() { 
+                FirstName = "Thierry", 
+                LastName = "Morre", 
+                Email = "thierry.morre@cognitic.be" 
+            });
+            model.AddAdmin(new Admin() { 
+                FirstName = "Michaël", 
+                LastName = "Person", 
+                Email = "michael.person@cognitic.be" 
+            });
+            model.AddAdmin(new Admin() { 
+                FirstName = "Aude", 
+                LastName = "Beurive", 
+                Email = "aude.beurive@bstorm.be" 
+            });
+            return View(model);
+        }
+
+        public string codehtml()
+        {
+            return @"<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Exemple de code html</title>
+    </head>
+    <body>
+        <h1>Exemple de code html</h1>
+    </body>
+</html>";
+        }
+
+        public int[] tableauEntier(int id=10)
+        {
+            int[] ints = new int[id];
+            for (int i = 0; i < ints.Length; i++)
+            {
+                ints[i] = i;
+            }
+            return ints;
+        }
+    }
+}
